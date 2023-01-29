527c5a41719bf593b735fbff9007fb8fbd14dd9a
citrix <2722279500@qq.com>
2023-01-29 09:00:57 +0800

    first commit

---
+++ src/Run.php
@@ -0,0 +1,4 @@
+<?php
+require "vendor/autoload.php";
+$response = new Pool\Cmp();
+$response->run();
---
+++ src/pool/Cmp.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pool;
+class Cmp
+{
+    function run()
+    {
+        echo "Form Test -> test() 99999999999";
+    }
+}
